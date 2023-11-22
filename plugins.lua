6cb0d094f74e43660fac546b601a2dbe7c8d9961
Raul Luzetti <raul.chiodi@gmail.com>
2023-11-22 15:16:38 -0300

    chore: added a new plugin

--- plugins.lua
+++ plugins.lua
@@ -114,11 +114,10 @@ local plugins = {
 
   {
     "iamcco/markdown-preview.nvim",
-    run = "cd app && npm install",
-    ft = "markdown",
-    config = function()
-      vim.g.mkdp_auto_start = 1
+    run = function()
+      vim.fn["mkdp#util#install"]()
     end,
+    ft = "markdown",
   },
 
   {
