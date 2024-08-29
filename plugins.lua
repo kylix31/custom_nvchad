3a6baf3e7e5b3bbfbec64b7e6d7faf0ab22f9458
Raul Luzetti <raul.chiodi@gmail.com>
2024-08-29 10:43:43 -0300

    chore: copilot removed

--- plugins.lua
+++ plugins.lua
@@ -303,18 +303,18 @@ local plugins = {
   --   "hrsh7th/nvim-cmp",
   --   opts = overrides.cmp,
   -- },
-  {
-    "jackMort/ChatGPT.nvim",
-    event = "VeryLazy",
-    config = function()
-      require "custom.configs.chatpgt"
-    end,
-    dependencies = {
-      "MunifTanjim/nui.nvim",
-      "nvim-lua/plenary.nvim",
-      "nvim-telescope/telescope.nvim",
-    },
-  },
+  -- {
+  --   "jackMort/ChatGPT.nvim",
+  --   event = "VeryLazy",
+  --   config = function()
+  --     require "custom.configs.chatpgt"
+  --   end,
+  --   dependencies = {
+  --     "MunifTanjim/nui.nvim",
+  --     "nvim-lua/plenary.nvim",
+  --     "nvim-telescope/telescope.nvim",
+  --   },
+  -- },
 
   -- {
   --   "mfussenegger/nvim-jdtls",
@@ -458,20 +458,20 @@ local plugins = {
   --   opts = require("custom.configs.cmp").config,
   -- },
 
-  {
-    "CopilotC-Nvim/CopilotChat.nvim",
-    lazy = false,
-    branch = "canary",
-    dependencies = {
-      { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
-      { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
-    },
-    opts = {
-      debug = true, -- Enable debugging
-      -- See Configuration section for rest
-    },
-    -- See Commands section for default commands if you want to lazy load on them
-  },
+  -- {
+  --   "CopilotC-Nvim/CopilotChat.nvim",
+  --   lazy = false,
+  --   branch = "canary",
+  --   dependencies = {
+  --     { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
+  --     { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+  --   },
+  --   opts = {
+  --     debug = true, -- Enable debugging
+  --     -- See Configuration section for rest
+  --   },
+  --   -- See Commands section for default commands if you want to lazy load on them
+  -- },
 
   -- {
   --   "pmizio/typescript-tools.nvim",
